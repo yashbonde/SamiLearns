6fe234717357e7ea3e2882ad2d000ffaca8ac10f
Yash Bonde <bonde.yash97@gmail.com>
2020-03-18 00:24:01 +0530

    complete FE and routing

---
+++ sami_learns/learning_system/book_handler.py
@@ -0,0 +1,11 @@
+"""this is the main handler for books. There are two main functions:
+- make new books
+- make configuration changes
+
+17.03.2020 - @yashbonde"""
+
+def make_new_book(queries):
+    pass
+
+def tune_book_parameters(book_id, section_id, tune_more_less):
+    pass
--- sami_learns/main.py
+++ sami_learns/main.py
@@ -1,12 +1,16 @@
 """main.py - file where the server resides
 08.03.2020 - @yashbonde"""
 
+# base dependencies
 import json
 from fastapi import FastAPI, Request # base imports
 from fastapi.staticfiles import StaticFiles # static files on the system
 from fastapi.templating import Jinja2Templates # jinja templates
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
+
+# custom dependencies
+from sami_learns.learning_system import book_handler
 
 # app things
 app = FastAPI()
@@ -28,10 +32,12 @@ def root(request: Request, cannot_login: bool = False):
 
 @app.get("/login")
 def login(request: Request, user: str, password: str):
-    user_name = user
+    username = user
     pwd_hash = password
 
-    if user_name not in fake_db or fake_db[user_name] != pwd_hash:
+    # return RedirectResponse('/{}'.format(username))
+
+    if username not in fake_db or fake_db[username] != pwd_hash:
         return templates.TemplateResponse("landing.html", {
             "request": request, 
             "cannot_login": True
@@ -39,15 +45,16 @@ def login(request: Request, user: str, password: str):
 
     else:
         # we have a user and he is authenticated
-        return templates.TemplateResponse("landing.html", {
-            "request": request, 
-            "cannot_login": False
-        })
+        return RedirectResponse('/{}'.format(username))
+
 
 @app.get("/{username}")
 def home_platform(request: Request, username: str):
     """take in the user_id and return the rendered template for
     homepage"""
+
+    # if username not in fake_db:
+    #     return RedirectResponse('/')
 
     documents = [
         {
@@ -61,16 +68,28 @@ def home_platform(request: Request, username: str):
 
     return templates.TemplateResponse("home.html", {
         "request": request,
-        "user_first_name": username,
+        "username": username,
         "documents": documents
     })
 
 # var regex_pat = RegExp('(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)');
 
-@app.get("/newBook")
-def make_new_book(request: Request):
-    pass
+class queriesModel(BaseModel):
+    queries: list
+    book_name: str
 
+@app.post("/{username}/newBook")
+def make_new_book(request: Request, username:str,  queries: queriesModel):
+    new_book_id = book_handler.make_new_book(book_id, section_id, tune_more_less)
+    new_book_id = 12
+
+    return {
+        "book_url": "/{username}/books/{book_id}".format(
+            username = username,
+            book_id = new_book_id
+        )
+    }
+    
 fake_books = {
     "12": json.load(open("sample_article_jsons/12.json")),
     "22": json.load(open("sample_article_jsons/22.json")),
@@ -81,5 +100,18 @@ def get_book(request: Request, username: str, book_id: int):
     return templates.TemplateResponse("book.html", {
         "request": request,
         "user_home_url": "/{username}".format(username = username),
+        "book_id_url": book_id,
+        "username": username,
         **fake_books[str(book_id)]
     })
+
+class BookConfigurationsModel(BaseModel):
+    more_less: str
+    book_id: str
+    section_id: str
+
+@app.post("/{username}/books/{book_id}")
+def tune_book(request: Request, username: str, book_id: int, book_config: BookConfigurationsModel):
+    print("---> {} --> {} --> {}".format(
+        book_config.more_less, book_config.book_id, book_config.section_id))
+    pass
